5308479c83393a539fb2d122676fbdea4b0cec09
Basil Alasadi <8195420+generic19@users.noreply.github.com>
2023-09-10 05:21:02 +0300

    Updated and organized wwwroot. Updated views: Home/Index, MyCourses/Index. Updated _Layout. Added _LoggedInLayout. Updated MyCoursesController. Added MyCourses/Sessions view. Added MyProfile controller and view.

--- KorsatkoApp/Controllers/MyCoursesController.cs
+++ KorsatkoApp/Controllers/MyCoursesController.cs
@@ -1,4 +1,5 @@
-﻿using KorsatkoApp.Data;
+﻿using KorsatkoApp.Areas.Admin.Models;
+using KorsatkoApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,8 +15,8 @@ namespace KorsatkoApp.Controllers {
 				.Include(e => e.student)
 				.Include(e => e.session)
 				.ThenInclude(e => e.course)
-				.Include(e => e.session)
-				.ThenInclude(e => e.instructor)
+				//.Include(e => e.session)
+				//.ThenInclude(e => e.instructor)
 				.Where(e => e.student.Email == User.Identity.Name).ToList();
 			return View(enrolls);
 		}
@@ -34,14 +35,24 @@ namespace KorsatkoApp.Controllers {
                 .ThenInclude(s => s.student)
                 .Where(s => s.Enrollments.Contains(enrollment)).First();
 
-
-
-
 			if (enrollment == null) {
 				return NotFound();
 			}
 
 			return View(session);
+		}
+
+		public async Task<IActionResult> Sessions()
+		{
+			IEnumerable<Enrollment> enrolls = await _context.Enrollments
+				.Include(e => e.student)
+				.Include(e => e.session)
+				.ThenInclude(e => e.course)
+				.Include(e => e.session)
+				.ThenInclude(e => e.instructor)
+				.Where(e => e.student.Email == User.Identity.Name).ToListAsync();
+
+			return View(enrolls);
 		}
 	}
 }
